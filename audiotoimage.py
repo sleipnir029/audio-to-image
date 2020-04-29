d72b27b2461f6192c3a95223b7dcbe765bfa88f5
romo1romo <52344836+sleipnir029@users.noreply.github.com>
2020-04-29 22:11:28 +0600

    final touches

--- audiotoimage.py
+++ audiotoimage.py
@@ -5,27 +5,27 @@ import wave
 import struct
 
 
-# opening sound file nd reading the binary data
-fname = "file_name.wav"
+# opening sound file and reading the binary data
+fname = "hellsbells.wav"
 w = wave.open(fname, 'rb')
 binary_data = w.readframes(w.getnframes())
 #print(binary_data)
 binary_data_len = len(binary_data)
-print(binary_data_len)
+#print(binary_data_len)
 w.close()
 
 
-# couning pixel and determining dimensions
+# counting pixel and determining dimensions
 total_pxl = int(binary_data_len / 4)
 xpxl = int(sqrt(total_pxl))
 ypxl = xpxl
-print('x:', xpxl, '\n', 'y:', ypxl) 
+#print(xpxl, ypxl) 
 
 
 # converting values from binary to integer
 int_data = struct.unpack(str(binary_data_len) + 'B', binary_data)
 int_data_len = len(int_data)
-print(int_data_len)
+#print(int_data_len)
 print("********Preparing**********")
 
 
