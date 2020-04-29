361b388481e00996e55b374acb4b256f84ac8f8d
romo1romo <52344836+sleipnir029@users.noreply.github.com>
2020-04-29 21:49:09 +0600

    reading binary data

--- audiotoimage.py
+++ audiotoimage.py
@@ -4,3 +4,12 @@ import random
 import wave 
 import struct
 
+
+# opening sound file nd reading the binary data
+fname = "file_name.wav"
+w = wave.open(fname, 'rb')
+binary_data = w.readframes(w.getnframes())
+#print(binary_data)
+binary_data_len = len(binary_data)
+#print(binary_data_len)
+w.close()
